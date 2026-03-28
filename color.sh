f12fbada1ccf2ca46b956bc185fcb618779df087
Caleb Fletcher <mcfletcher99@gmail.com>
2026-03-28 03:57:23 +0000

    Fixed ascii art bug

--- color.sh
+++ color.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 COLOR='\033[0;31m'
+NORMAL='\033[0m'
 echo -e "${COLOR}    |  |    "
 echo -e "${COLOR}  *      *  "
-echo -e "${COLOR}   ******   "
+echo -e "${COLOR}   ******  ${NORMAL}"
